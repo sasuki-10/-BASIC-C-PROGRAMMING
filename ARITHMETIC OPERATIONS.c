fe22cb7a045d08a1ced530fbe4ab9d6c1e1ae116
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-02 23:47:24 +0530

    Create ARITHMETIC OPERATIONS.c

---
+++ ARITHMETIC OPERATIONS.c
@@ -0,0 +1,50 @@
+// Program to print the arithmetic operations
+#include <stdio.h>
+int main()
+{
+    int a=20;
+    int b=10;
+    int c;
+    c=a+b;
+    printf("THE ADDITION VALUE OF c:%d",c);
+    
+    c=a*b;
+    printf("\nTHE MULTIPLICATION VALUE OF c:%d",c);
+   
+    c=a/b;
+    printf("\nTHE DIVISION VALUE OF c:%d ",c);
+   
+    c=a-b;
+    printf("\nTHE SUBSTRACTION VALUE OF c:%d ",c);
+   
+    return 0;
+}
+                                      "2nd method"
+
+// Program to print the arithmetic operations
+#include <stdio.h>
+int main()
+{
+    int a,b;
+   printf("ENTER THE VALUE OF a:%d");
+   scanf("%d",&a);
+   
+    printf("ENTER THE VALUE OF b:%d");
+    scanf("%d",&b);
+
+
+    int c;
+    c=a+b;
+    printf("THE ADDITION VALUE OF c:%d",c);
+    
+    c=a*b;
+    printf("\nTHE MULTIPLICATION VALUE OF c:%d",c);
+   
+    c=a/b;
+    printf("\nTHE DIVISION VALUE OF c:%d ",c);
+   
+    c=a-b;
+    printf("\nTHE SUBSTRACTION VALUE OF c:%d ",c);
+   
+    return 0;
+}
