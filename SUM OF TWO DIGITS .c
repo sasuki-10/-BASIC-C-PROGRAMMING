ba75cf133ebc27f0cc94b2c4ce23ba2de0ea4666
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-02 23:33:42 +0530

    SUM OF TWO DIGITS .c

---
+++ SUM OF TWO DIGITS .c
@@ -0,0 +1,18 @@
+// Program to print the sum to two number
+#include <stdio.h>
+int main()
+{
+    int a=10;
+    int b=20;
+    int sum;
+    printf("THE VALUE OF a:%d",a);
+    
+    printf("\nTHE VALUE OF b:%d",b);
+   
+    sum=a+b;
+    
+    printf("\nTHE VALUE OF SUM:%d ",sum);
+   
+   
+    return 0;
+}
