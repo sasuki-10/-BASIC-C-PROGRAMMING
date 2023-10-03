b307906ba192cd939e23675ff1d4dc8808b5cd10
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-03 18:04:34 +0530

    INCREMENT & DECREMENT(2).c

---
+++ INCREMENT &DECREMENT(2).c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int a=10;
+	
+	// preincrement.......
+	
+	int b=++a;
+	printf("value pf a %d",b);
+	
+	// predecrement.......
+	
+    int c=--a;
+	printf("\nvalue pf a %d",c);
+	
+	// postincrement.......
+	
+	int d=a++;
+	printf("\nvalue pf a %d",d);
+	
+	// postdecrement.......
+	
+	int e=a--;
+	printf("\nvalue pf a %d",e);
+}	
