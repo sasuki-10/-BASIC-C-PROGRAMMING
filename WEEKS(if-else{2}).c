cb430c2f9f1d145ae2a255dd52e417022e4fcd24
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-04 22:51:54 +0530

    WEEKS(if-else{2}).c

---
+++ WEEKS(if-else{2}).c
@@ -0,0 +1,41 @@
+//checking if-else condition..
+#include<stdio.h>
+int main()
+{
+ int num;
+ printf("ENTER YOUR NUMBER: ");
+ scanf("%d",&num);
+ if(num==1)
+ {
+     printf("sunday");
+ }
+ else if(num==2)
+ {
+     printf(" monday");
+ }
+ else if(num==3)
+ {
+     printf(" tuesday");
+ }
+ else if(num==4)
+ {
+     printf(" wednesday");
+ }
+ else if(num==5)
+ {
+     printf(" thursday");
+ }
+ else if(num==6)
+ {
+     printf(" friday");
+ }
+ else if(num==7)
+ {
+     printf(" saturday");
+ }
+ else;
+ {
+     printf(" not in week");
+ }
+ return 0;
+}
