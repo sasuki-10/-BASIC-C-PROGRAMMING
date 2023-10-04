f2fb4d55dd3a2b58cae854e64b697f4ecc8efe45
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-04 22:46:10 +0530

    if-else(1).c

---
+++ if-else(1).c
@@ -0,0 +1,18 @@
+//checking if-else condition..
+#include<stdio.h>
+int main()
+{
+ int age;
+ printf("ENTER YOUR AGE TO CHECK WHETHER YOU ARE ELLIGIBLE TO VOTE OR NOT: ");
+ scanf("%d",&age);
+ if(age>=18)
+ {
+     printf("elligible to vote;");
+ }
+ else;
+ {
+     printf(" not elligible to vote;");
+ }
+ 
+ return 0;
+}
