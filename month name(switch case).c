4fd110d48a2bdbd6264ce9ba9a91c350630121e8
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-04 23:42:52 +0530

    month name(switch case).c

---
+++ month name(switch case).c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("\nEnter the num:");
+	scanf(" %d",&num);
+
+	switch(num)
+{
+	case 1:printf("january");
+		break;
+		case 2:printf("Februray");
+		break;
+		case 3:printf("March");
+		break;
+	    case 4:printf("april");
+		break;
+		case 5:printf("may");
+		break;
+	    case 6:printf("june");
+		break;
+		case 7:printf("july");
+		break;
+		case 8:printf("august");
+		break;
+		case 9:printf("september");
+		break;
+		case 10:printf("october");
+		break;
+		case 11:printf("november");
+		break;
+		case 12:printf("december");
+		break;
+	default:printf(" error");	
+	}	
+}	
